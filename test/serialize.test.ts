c85c0ff06da6b34d5b9c3f16c666ac24bd743cf7
Etienne Dldc <e.deladonchamps@gmail.com>
2019-12-06 09:14:17 +0100

    Add more tests

--- test/serialize.test.ts
+++ test/serialize.test.ts
@@ -1,9 +1,18 @@
 import { Serializer } from '../src';
 
-test('Handle escaped characteres in serialize', () => {
+test('Handle quote in string when serialize', () => {
   expect(() => Serializer.serialize('"')).not.toThrow();
   expect(() => Serializer.serialize("'")).not.toThrow();
 
   expect(Serializer.serialize('"')).toEqual(`'"'`);
   expect(Serializer.serialize("'")).toEqual(`"'"`);
+});
+
+test('Handle escaped quote in string', () => {
+  // eslint-disable-next-line prettier/prettier
+  // prettier-ignore
+  expect(() => Serializer.serialize("\"")).not.toThrow();
+  // eslint-disable-next-line prettier/prettier
+  // prettier-ignore
+  expect(Serializer.serialize("\"")).toEqual(`'"'`);
 });
